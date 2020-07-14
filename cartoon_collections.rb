1625e68c5a967d0a065eeeb1073d2b84d17a8c82
Abdullah Albasha <Abdullah.a.albasha@gmail.com>
2020-07-14 15:51:56 -0400

    cartoon collection lab completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+require 'pry'
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
+def roll_call_dwarves(array)# code an argument here
+  # Your code here
+  array.each_with_index do |dwarf, index|
+    puts "#{index +1}. #{dwarf} "
+  end
+end
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+def summon_captain_planet(array)# code an argument here
+  # Your code here
+  planeteer_calls = array.map do |element|
+  element.capitalize + "!"
+  end
+  print planeteer_calls
+end
+summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+
+def long_planeteer_calls(array)# code an argument here
+  # Your code here
+puts array.any? {|element| element.length > 4 }
+end
+long_planeteer_calls(["two", "go", "industrious", "bop"])
+
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include? (cheese)
+  end
 end
+# find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
